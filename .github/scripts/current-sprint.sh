480c7c6f48c5b9fbb7596162124ff2f48f4ed42f
ariyn <himnowxz@gmail.com>
2021-03-07 14:57:15 +0900

    edit

--- .github/scripts/current-sprint.sh
+++ .github/scripts/current-sprint.sh
@@ -88,7 +88,7 @@ if [[ $LATEST_SPRINT_TAG -ne $LATEST_SPRINT ]]; then
 	echo $EXISTS_DEPLOY_BRANCH;
 	echo $REMOTE_EXISTS_DEPLOY_BRANCH;
 	
-	if ([[ -z "$EXISTS_DEPLOY_BRANCH" ]] && [[ -z "$REMOTE_EXISTS_DEPLO_BRANCH" ]]); then
+	if [ -z "$EXISTS_DEPLOY_BRANCH" ] && [ -z "$REMOTE_EXISTS_DEPLOY_BRANCH" ]; then
 		echo "deploy branch ${DEPLOY_BRANCH_KEY} not exists"
 		echo "create new branch"
 		
